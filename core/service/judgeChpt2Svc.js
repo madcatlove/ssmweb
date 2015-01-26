5e26bf49d38b648fb085190724c652275f3cafe4
junhojang <jangjunho7@gmail.com>
2015-01-27 02:28:30 +0900

    - Changed Judge Function Structure

--- core/service/judgeChpt2Svc.js
+++ core/service/judgeChpt2Svc.js
@@ -2,9 +2,46 @@
  * Created by jangjunho on 15. 1. 26..
  */
 
+var jMath = require('../Math');
+
 var judgeChpt2 = {
 
     rect : function (data, callback) {
+        var dummy = [
+            // CCW
+            [-1.0, 1.0, 1.0],
+            [-1.0, -1.0, 1.0],
+            [1.0, -1.0, 1.0],
+            [1.0, 1.0, 1.0],
+
+            // CW
+            [-1.0, 1.0, -1.0],
+            [-1.0, -1.0, -1.0],
+            [1.0, -1.0, -1.0],
+            [1.0, 1.0, -1.0],
+
+        ];
+
+        var normals = [
+            jMath.normal3f([ dummy[0], dummy[1], dummy[2], dummy[3] ]),
+            jMath.normal3f([ dummy[4], dummy[7], dummy[6], dummy[5] ]),
+
+            jMath.normal3f([ dummy[4], dummy[0], dummy[3], dummy[7] ]),
+            jMath.normal3f([ dummy[5], dummy[6], dummy[2], dummy[1] ]),
+
+            jMath.normal3f([ dummy[3], dummy[2], dummy[6], dummy[7] ]),
+            jMath.normal3f([ dummy[0], dummy[4], dummy[5], dummy[1] ])
+        ];
+
+        var res = [0, 0, 0];
+
+        for (var i = 0 ; i < normals.length ; i ++) {
+            for (var j = 0 ; j < 3; j ++) {
+                res[j] += normals[i][j];
+            }
+        }
+
+        return (res[0] == 0) && (res[1] == 0) && (res[2] == 0);
 
     },
 
@@ -13,7 +50,7 @@ var judgeChpt2 = {
     },
 
     circle : function(data, callback) {
-
+        var dummy = [0.0, 0.0, 0.0, 5];
 
     },
 
