c29c758b7d888c4d9fd2c2ab61c82660094de85e
madcat <kr.madcat@gmail.com>
2015-01-28 23:03:41 +0900

    삽질중.

--- static/js/board.js
+++ static/js/board.js
@@ -44,6 +44,7 @@ sBoard.prototype.retriveData = function() {
         console.log(self.data);
 
         self.refine();
+        self.render();
     })
 
 }
@@ -74,3 +75,93 @@ sBoard.prototype.refine = function() {
     console.log( this.parentArticle );
     console.log( this.childArticle );
 }
+
+sBoard.prototype.render = function() {
+    for( var key in this.parentArticle ) {
+        // -- append parent --
+        $('#parentList').tmpl( this.parentArticle[key] ).appendTo( $('.panel-group'));
+
+
+        //-- append child --
+        if( this.childArticle.hasOwnProperty(key) ) {
+            for( var i = 0; i < this.childArticle[key].length; i++) {
+                 $('#childList').tmpl( this.childArticle[key][i] ).appendTo( $('#collapse'+key+' .panel-body') );
+            }
+        }
+
+        // badge 갯수 업데이트
+        var countComment = (this.childArticle.hasOwnProperty(key)) ? this.childArticle[key].length : 0;
+        $('#parentCollapse'+key+ ' span.badge').html( countComment );
+    }
+
+    //-- 모든 작업이 끝나면 아코디언을 닫은 상태로 바꾼다 --
+    $('button[data-seq]').click( function(e) {
+        var dataSeq = $(this).attr('data-seq');
+
+        $('#replyArticleModal h4 span').html('#' + dataSeq );
+
+        $('#replyArticleModal').modal({
+           keyboard: false,
+           backdrop: 'static',
+           show: true,
+        });
+    })
+
+}
+
+sBoard.prototype.writeArticle = function(tid) {
+    var articleContent = $('#newArticleContent');
+
+    if( articleContent.val().trim().length == 0 ) {
+        alert(' 내용을 작성하셔야 합니다. ');
+        articleContent.focus();
+        return false;
+    }
+
+    var sParam = {
+        parentSeq : -1,
+        tid : tid,
+        content : articleContent.val().trim(),
+    }
+
+    urlReq.post('/board/'+tid, sParam, function(result) {
+        if( result.error ) {
+            alert(' 질문 작성 도중 에러가 발생하였습니다. ');
+            console.log( result.data );
+            return false;
+        }
+        alert(' 질문을 등록하였습니다. ');
+        window.location.reload();
+    })
+
+}
+
+sBoard.prototype.writeReplyArticle = function(tid) {
+
+    var articleContent = $('#replyArticleModal .modal-body textarea');
+    var parentSeq = parseInt( $('#reply'))
+
+    if( articleContent.val().trim().length == 0 ) {
+        alert(' 내용을 작성하셔야 합니다. ');
+        articleContent.focus();
+        return false;
+    }
+
+    var sParam = {
+        parentSeq : ,
+        tid : tid,
+        content : articleContent.val().trim(),
+    }
+
+    urlReq.post('/board/'+tid, sParam, function(result) {
+        if( result.error ) {
+            alert(' 질문 작성 도중 에러가 발생하였습니다. ');
+            console.log( result.data );
+            return false;
+        }
+        alert(' 질문을 등록하였습니다. ');
+        window.location.reload();
+    })
+
+}
+
