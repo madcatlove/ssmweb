eec447071750c04786064ed05f0d9cc2caa8f3d0
madcat <kr.madcat@gmail.com>
2015-01-29 10:13:42 +0900

    게시판 목록 / 페이지네이션 완료.

--- static/js/board.js
+++ static/js/board.js
@@ -8,8 +8,11 @@ function sBoard() {
     this.countOffset = 10;
     this.data = null;
 
-    this.parentArticle = {}
-    this.childArticle = {}
+    this.parentArticle = [];
+    this.childArticle = {};
+    this.boardInfo = null;
+    this.countOffset = 10; // 한페이지에 보여줄 게시물 갯수.
+    this.blockOffset = 5; // 한페이지에 보여줄 페이지 블락 갯수.
 }
 
 sBoard.prototype.setTutorialSeq = function(tutorialSeq) {
@@ -34,17 +37,36 @@ sBoard.prototype.retriveData = function() {
 
     var self = this;
     var bUrl = '/board/' + (this.tutorialSeq) + '/' + (this.page);
+    var infoUrl = '/board/' + (this.tutorialSeq) + '/info';
+    var bParam = {
+        countOffset : this.countOffset,
+    }
 
-    urlReq.get( bUrl, {}, function(result) {
+    /* 게시글 정보 가져오기 */
+    urlReq.get( bUrl, bParam , function(result) {
         if( result.error ) {
             alert(' 에러가 발생하였습니다. ');
             return false;
         }
-        self.data = result.data;
-        console.log(self.data);
 
-        self.refine();
-        self.render();
+        /* 게시판 정보 가져오기 */
+        urlReq.get(infoUrl, {}, function(infoResult) {
+            if( infoResult.error ) {
+                alert(' 에러가 발생하였습니다. ');
+                console.log( infoResult );
+                return false;
+            }
+
+
+            /* 데이터 주입 작업 */
+            self.data = result.data;
+            self.boardInfo = infoResult.data;
+
+            self.refine();
+            self.render();
+            self.createPagination();
+        })
+
     })
 
 }
@@ -52,13 +74,14 @@ sBoard.prototype.retriveData = function() {
 sBoard.prototype.refine = function() {
     var s = this.data;
 
+    console.log( 'data' , this.data );
+
+
     for(var i = 0; i < s.length; i++) {
         var item = s[i];
 
         if( item.seq == item.parentSeq ) { // 부모글 이라면.
-            if( !this.parentArticle.hasOwnProperty( item.seq) ) {
-                this.parentArticle[item.seq] = item;
-            }
+            this.parentArticle.push( item );
         }
         // 부모글이 아니라면 자식글로 append
         else {
@@ -72,26 +95,26 @@ sBoard.prototype.refine = function() {
         }
     }
 
-    console.log( this.parentArticle );
-    console.log( this.childArticle );
 }
 
 sBoard.prototype.render = function() {
     for( var key in this.parentArticle ) {
+        var parentSeq = this.parentArticle[key].seq;
+
         // -- append parent --
         $('#parentList').tmpl( this.parentArticle[key] ).appendTo( $('.panel-group'));
 
 
         //-- append child --
-        if( this.childArticle.hasOwnProperty(key) ) {
-            for( var i = 0; i < this.childArticle[key].length; i++) {
-                 $('#childList').tmpl( this.childArticle[key][i] ).appendTo( $('#collapse'+key+' .panel-body') );
+        if( this.childArticle.hasOwnProperty(parentSeq) ) {
+            for( var i = 0; i < this.childArticle[parentSeq].length; i++) {
+                 $('#childList').tmpl( this.childArticle[parentSeq][i] ).appendTo( $('#collapse'+parentSeq+' .panel-body') );
             }
         }
 
         // badge 갯수 업데이트
-        var countComment = (this.childArticle.hasOwnProperty(key)) ? this.childArticle[key].length : 0;
-        $('#parentCollapse'+key+ ' span.badge').html( countComment );
+        var countComment = (this.childArticle.hasOwnProperty(parentSeq)) ? this.childArticle[parentSeq].length : 0;
+        $('#parentCollapse'+parentSeq+ ' span.badge').html( countComment );
     }
 
     //-- 모든 작업이 끝나면 아코디언을 닫은 상태로 바꾼다 --
@@ -99,6 +122,7 @@ sBoard.prototype.render = function() {
         var dataSeq = $(this).attr('data-seq');
 
         $('#replyArticleModal h4 span').html('#' + dataSeq );
+        $('#replyArticleModal').attr('data-parentSeq', $(this).attr('data-seq') );
 
         $('#replyArticleModal').modal({
            keyboard: false,
@@ -148,20 +172,65 @@ sBoard.prototype.writeReplyArticle = function(tid) {
     }
 
     var sParam = {
-        parentSeq : ,
+        parentSeq : $('#replyArticleModal').attr('data-parentSeq'),
         tid : tid,
         content : articleContent.val().trim(),
     }
 
     urlReq.post('/board/'+tid, sParam, function(result) {
         if( result.error ) {
-            alert(' 질문 작성 도중 에러가 발생하였습니다. ');
+            alert(' 답변 작성 도중 에러가 발생하였습니다. ');
             console.log( result.data );
             return false;
         }
-        alert(' 질문을 등록하였습니다. ');
+        alert(' 답변을 등록하였습니다. ');
         window.location.reload();
     })
 
+}
+
+sBoard.prototype.createPagination = function() {
+    var totalArticle = this.boardInfo.parentCount;
+    var numPages = Math.ceil( 1.0 * totalArticle / this.countOffset );
+    var numBlocks = Math.ceil( numPages / this.blockOffset );
+    var sPath =  window.location.pathname + '?page=';
+
+
+    // 현재페이지의 속한 블락 구간을 구함.
+    var currentBlock = parseInt( Math.ceil( this.page / this.blockOffset ) );
+
+    // 현재 블락의 시작페이지 구함.
+    var currentBlockStartPage = (currentBlock -1) * this.blockOffset + 1;
+
+    var html = '';
+    if( this.page == 1 ) {
+        html += '<li><a href="#">«</a></li>';
+    }
+    else {
+        html += '<li><a href="'+(sPath + (this.page-1))+'">«</a></li>';
+    }
+    for( var i = 0; i < this.blockOffset; i++) {
+
+        /* 마지막 페이지보다 페이지 계산이 더 커버리면 중단 */
+        if( ( currentBlockStartPage + i ) > numPages ) {
+            break;
+        }
+
+        if( i+ currentBlockStartPage == this.page ) {
+            html += '<li class="active"><a href="#">'+(i+currentBlockStartPage)+'</a></li>';
+        }
+        else {
+            html += '<li><a href="'+(sPath + (i+currentBlockStartPage))+'">'+(i+currentBlockStartPage)+'</a></li>';
+        }
+
+    }
+    if( this.page == numPages ) {
+        html += '<li><a href="#">»</a></li>';
+    }
+    else {
+        html += '<li><a href="'+(sPath + (this.page+1))+'">»</a></li>';
+    }
+
+    $('#sPagination ul').append(html);
 }
 
