c6ccc866593983d862f4236e518b658310c6004c
madcat <kr.madcat@gmail.com>
2015-02-04 02:43:00 +0900

    내부 인트라넷 서버로 이전

--- config/mysqlConfig.js
+++ config/mysqlConfig.js
@@ -8,7 +8,15 @@
  */
 
 module.exports = {
+    /*
     host : 'ssm.dude.kr',
+    port : 3306,
+    userid : 'ssm',
+    userpwd : 'ssm2014',
+    userdb : 'ssm',
+    dbPoolSize : 10
+    */
+    host : '211.189.20.28',
     port : 3306,
     userid : 'ssm',
     userpwd : 'ssm2014',
