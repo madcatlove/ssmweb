cbd4cf7d8e0f07eef2aa569d984debfc11566554
madcat <kr.madcat@gmail.com>
2015-02-13 09:16:34 +0900

    .

--- core/route/member.js
+++ core/route/member.js
@@ -19,12 +19,9 @@ router.use( function(req, res, next) {
 
 /* Routing */
 router.post('/login', memberController.procLogin); // 로그인 처리.
-
+router.get('/logout', memberController.procLogout); // 로그아웃 처리.
 router.get('/join', memberController.renderJoin); // 회원가입 페이지
 router.post('/join', memberController.procJoin); // 회원가입 처리
-
-router.get('/logout', memberController.procLogout); // 로그아웃 처리.
-
 router.put('/clearTutorial', memberController.clearTutorialGuide); // 가이드 클리어
 
 
