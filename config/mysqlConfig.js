9be6d815b1a6ff232783ea4014ab0f345669a8c2
madcat <kr.madcat@gmail.com>
2015-02-24 23:58:18 +0900

    121.165.217.137 서버 환경설정 변경

--- config/mysqlConfig.js
+++ config/mysqlConfig.js
@@ -16,7 +16,16 @@ module.exports = {
     userdb : 'ssm',
     dbPoolSize : 10
     */
+
+    /*
     host : '211.189.20.28',
+    port : 3306,
+    userid : 'ssm',
+    userpwd : 'ssm2014',
+    userdb : 'ssm',
+    dbPoolSize : 10,
+    */
+    host : '121.165.217.137',
     port : 3306,
     userid : 'ssm',
     userpwd : 'ssm2014',
